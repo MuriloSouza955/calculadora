fabeeec9311865024b8d11ae8f983fb7fa7ad56b
MuriloSouza955 <107726242+MuriloSouza955@users.noreply.github.com>
2022-08-04 18:30:38 -0300

    aula18 - Implementação do Teclado

--- src/br/com/MKCM/calc/visao/Botao.java
+++ src/br/com/MKCM/calc/visao/Botao.java
@@ -1,6 +1,15 @@
 package br.com.MKCM.calc.visao;
 
 import javax.swing.*;
-
+import java.awt.*;
 public class Botao extends JButton {
+
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setFont(new Font("couruer", Font.PLAIN, 25));
+        setOpaque(true);
+        setBackground(cor);
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
 }
--- src/br/com/MKCM/calc/visao/Teclado.java
+++ src/br/com/MKCM/calc/visao/Teclado.java
@@ -1,10 +1,35 @@
 package br.com.MKCM.calc.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Teclado extends JPanel {
 
     public Teclado() {
+        setLayout(new GridLayout(5, 4));
 
+        Color COR_CINZA_ESCURO = new Color(68, 68, 68);
+        Color COR_LARANJA = new Color(242, 163, 60);
+        Color COR_CINZA_CLARO = new Color(99, 99, 99);
+
+        add(new Botao("AC", COR_CINZA_ESCURO));
+        add(new Botao("+/-", COR_CINZA_ESCURO));
+        add(new Botao("%", COR_CINZA_ESCURO));
+        add(new Botao("/", COR_LARANJA));
+
+        add(new Botao("7", COR_CINZA_CLARO));
+        add(new Botao("8", COR_CINZA_CLARO));
+        add(new Botao("9", COR_CINZA_CLARO));
+        add(new Botao("*", COR_LARANJA));
+
+        add(new Botao("7", COR_CINZA_CLARO));
+        add(new Botao("8", COR_CINZA_CLARO));
+        add(new Botao("9", COR_CINZA_CLARO));
+        add(new Botao("*", COR_LARANJA));
+
+        add(new Botao("7", COR_CINZA_CLARO));
+        add(new Botao("8", COR_CINZA_CLARO));
+        add(new Botao("9", COR_CINZA_CLARO));
+        add(new Botao("*", COR_LARANJA));
     }
 }
